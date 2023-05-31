88e9eddc738233e94e7d048342c86ea4d713722c
r-tarnowski <radek.github@wp.pl>
2023-06-01 00:22:51 +0200

    Initial commit

---
+++ src/MotivatingExample.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+using std::cout;
+using std::endl;
+
+void printHeader() {
+   cout << endl;
+   cout << "================================================" << endl;
+   cout << "  C++20 The Complete Guide by Nicolai Josuttis  " << endl;
+   cout << "                  Concepts                      "<< endl;
+   cout << "================================================" << endl;
+   cout << endl;
+}
+
+
+
+int main(int argc, char *argv[]) {
+   printHeader();
+
+   return 0;
+}
