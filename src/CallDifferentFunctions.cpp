7885b366339cae0a1e088b15475f7b97c12f0767
r-tarnowski <radek.github@wp.pl>
2023-06-11 21:46:41 +0200

    Examples from Concepts for if constexpr and Inserting Single and Multiple Values added. Do NOT work!

--- src/CallDifferentFunctions.cpp
+++ src/CallDifferentFunctions.cpp
@@ -37,8 +37,18 @@ concept SupportsPushBack_WithDeclvalAndRanges = requires( Coll c ) {
     c.push_back( std::declval<std::ranges::range_value_t<Coll>>() );
 };
 
+/*
 template<typename Coll, typename T>
 requires SupportsPushBack_WithOneParameter<Coll>
+void add( Coll & coll, const T & val ) {
+    std::cout << "Method add for generic value types that supports push_back called" << std::endl;
+    coll.push_back( val );
+}
+*/
+
+
+template<typename Coll, typename T>
+requires SupportsPushBack_WithDeclval<Coll>
 void add( Coll & coll, const T & val ) {
     std::cout << "Method add for generic value types that supports push_back called" << std::endl;
     coll.push_back( val );
@@ -49,6 +59,34 @@ void add( Coll & coll, const T & val ) {
     std::cout << "Method add for generic value types that DOES NOT support push_back called" << std::endl;
     coll.insert( val );
 }
+
+/*
+ Doesn't work. TO DO: check why. 
+template<typename Coll, std::ranges::input_range T>
+requires SupportsPushBack_WithDeclval<Coll>
+void add( Coll & coll, const T & val ) {
+    std::cout << "Method add for generic value types that supports push_back and is specialized for ranges called" << std::endl;
+    coll.insert( coll.end(), std::ranges::begin( val ), std::ranges::end( val ) );
+}
+
+template<typename Coll, std::ranges::input_range T>
+void add( Coll & coll, const T & val ) {
+    std::cout << "Method add for generic value types that DOSE NOT support push_back but is specialized for ranges called" << std::endl;
+    coll.insert( val.begin(), val.end() );
+}
+
+
+Doesn't work. TO DO: check why.
+template<typename Coll, typename T>
+void add( Coll & coll, const T & val ) {
+    if constexpr (SupportsPushBack_WithOneParameter<decltype(coll)>) {
+        coll.push_back( val );
+    } else {
+       coll.insert( val );
+   }
+}
+*/
+
 
 int main(int argc, char *argv[]) {
     printHeader();
@@ -63,8 +101,12 @@ int main(int argc, char *argv[]) {
     add( iList, intVal );
 
     std::map<int, int> iMap;
-    std::cout  << "add( iMap, (intVal, intVal ) -> ";
+    std::cout  << "add( iMap, (intVal, intVal ) ) -> ";
     add( iMap, std::make_pair( 1, 2 ) );
+
+    std::vector< std::vector<int> > vecOfIntVec = { { 1 }, { 1, 2 }, { 1, 2, 3 } };
+    std::cout  << "add( vecOfIntVec, iVec ) -> ";
+    add( vecOfIntVec, iVec );
 
     return 0;
 }
